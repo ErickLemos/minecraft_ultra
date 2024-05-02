99d0b446e4bd953a5cd80eaf8c82d8856e459c7e
Erick Lemos <ericklemoscosta@gmail.com>
2024-05-01 23:25:00 -0300

    setup: compilação precoce

--- start.sh
+++ start.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/env bash
-./jdk/bin/java -Xmx2G -jar fabric-server-launch.jar nogui
+./jdk/bin/java \
+    -XX:MaxRAMPercentage=40.0 \
+    -XX:+UseG1GC -XX:MaxGCPauseMillis=100 \
+    -XX:CompileThreshold=1000 \
+    -XX:C1CompileThreshold=1000 \
+    -XX:C2CompileThreshold=1000 \
+    -XX:FalconCompileThreshold=1000 \
+    -jar fabric-server-launch.jar nogui
