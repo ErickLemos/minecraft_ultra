7eb0e3dbeba37ae59990785608860c8d33651d90
Erick Lemos <ericklemoscosta@gmail.com>
2024-05-02 17:35:20 -0300

    feature: z clear

--- start.debug.sh
+++ start.debug.sh
@@ -7,5 +7,7 @@
     -XX:C2CompileThreshold=1000 \
     -XX:FalconCompileThreshold=1000 \
     -XX:+FlightRecorder \
-    -XX:StartFlightRecording=duration=5m,filename=flight.jfr \
+    -XX:StartFlightRecording=duration=5m,filename=flight-optimized.jfr \
     -jar fabric-server-launch.jar nogui
+
+source ./z-clear.sh
---
+++ z-clear.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+sudo rm -r .fabric
+sudo rm -r logs
+sudo rm -r world
